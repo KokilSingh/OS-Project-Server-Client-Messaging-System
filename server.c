f7082725dc8fba7cfd95c6152769cfcb0137bf66
Kokil Singh <87122822+KokilSingh@users.noreply.github.com>
2023-09-28 17:04:28 +0530

    Its working
    Cleanup remaining

--- server.c
+++ server.c
@@ -1,112 +1,161 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ipc.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <string.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<stdbool.h>
 
 #define MAX_MTEXT_SIZE 100
 
-// Structure for MQ
-struct message {
-    long msgtype; // should hold client Id
-    char mtext[100];
+struct message{
+    long mtype;
+    int oprid;
+    char mfile[100];
 };
 
+
 int main() {
-    
-    // The important stuff: Initialization of variables and Message queue declaration
     key_t key;
     int msgqid;
-    struct message msg;
-   
-    long clientID;
-    
-    key = ftok(".", 'a');
-    msgqid = msgget(key, 0666 | IPC_CREAT);
+    key = ftok(".",'a');
+    msgqid = msgget(key, 0664 | IPC_CREAT);
 
-    if (msgqid == -1) {
-        perror("msgget");
-        exit(EXIT_FAILURE);
-    }
-	
-    // Listen for client requests
-    while(1)
-    {
-    
-    	printf("Listening for messages\n");
-    	
-    	// Read whichever message is 1st in queue (kataar)
-    	if(msgrcv(msgqid,&msg,MAX_MTEXT_SIZE,0,0)==-1)
-    	{
-    		perror("msgrcv");
-    		exit(EXIT_FAILURE);
-    	}
-    	
-    	clientID=msg.msgtype;
-    	printf("Message received from client: %ld\n",msg.msgtype);
-    	
-    	// Now that message is received, lets (drumrolls....) FORK IT
-    	pid_t p=fork();
-    	if(p==-1)
-    	{
-    		perror("fork");
-    		exit(EXIT_FAILURE);
-    	}
-    	else if(p==0)
-    	{
-    		char response[100];
-    		
-    		// Bachha Process
-    		
-    		// Choice Handling
-    		// Req: Need to create separate pipes for parent child communication
-    		if(strcmp("4",msg.mtext)==0)
-    		{
-    			strcpy(response,"Good bye");
-    		}
-    		else if(strcmp("Hi",msg.mtext)==0)
-    		{
-    			strcpy(response,"Helloooo");
-    		}
-    		else
-    		{
-    			// Req: Need to creat separate path for choice 2 and 3
-    			strcpy(response,"Middle choice");
-    		}
-    		
-    		// Send messages to client via message queue
-    		
-    		strcpy(msg.mtext,response);
-    		
-    		if(msgsnd(msgqid,&msg,sizeof(msg.mtext),0)==-1)
-    		{
-    			//Error in sending Message
-    			perror("msgsnd");
-    			exit(EXIT_FAILURE);
-    		}
-    	
-    		printf("Message sent to client: %ld\n\n",msg.msgtype);
-    		
-    		exit(EXIT_SUCCESS);
-    	}
-    	else
-    	{
-    		// Maa Process
-    		wait(NULL);
-    	}
-    	
-    }
+    int pipe_fd[2]; // Array to hold file descriptors for the pipe
 
-    // Cleanup and exit
-    msgctl(msgqid, IPC_RMID, NULL); // Remove the message queue
-    printf("Client %ld exiting...\n", clientID);
-    exit(EXIT_SUCCESS);
+
+    while(true){
+
+
+        struct message nig; // Buffer to hold data
+        msgrcv(msgqid,&nig,MAX_MTEXT_SIZE,0,0);
+
+        // Create the pipe
+        if (pipe(pipe_fd) == -1) {
+            perror("Pipe creation failed at line 11 in server.c\n");
+            exit(-1);
+        }
+
+        // Create a child process (client)
+        pid_t child_pid = fork();
+
+        if (child_pid == -1) 
+        {
+            perror("Fork failed at line 19 in server.c");
+            exit(EXIT_FAILURE);
+        }
+
+        if (child_pid == 0) 
+        {
+            // Child process (client)
+            close(pipe_fd[1]); // Close the write end of the pipe (not needed by the child)
+
+            // Read data from the pipe
+            //printf("Receiving message...\n");
+            read(pipe_fd[0], &nig, sizeof(nig));
+            if(nig.oprid == 1){
+                strcpy(nig.mfile, "Hello");
+                msgsnd(msgqid, &nig, sizeof(nig.mfile), 0);
+            }
+
+            close(pipe_fd[0]); // Close the read end of the pipe (client is done reading)
+
+
+            /*  COPY PASTED TEMP.C*/
+            int pipe_fd_1[2];
+            pid_t child_pid_1;
+            if (pipe(pipe_fd_1) == -1) {
+                perror("Pipe creation failed");
+                exit(EXIT_FAILURE);
+            }
+
+            // Create a child process
+            child_pid_1 = fork();
+
+            if (child_pid_1 == -1) {
+                perror("Fork failed");
+                exit(EXIT_FAILURE);
+            }
+
+            if (child_pid_1 == 0) {   // This is the server->child->child process 
+                // Child process
+                close(pipe_fd_1[0]); // Close the read end of the pipe (not needed by the child)
+
+                // Redirect stdout to the write end of the pipe
+                dup2(pipe_fd_1[1], 1);
+                close(pipe_fd_1[1]); // Close the original write end of the pipe
+                const char *fileName = nig.mfile;
+                // Replace the child process with a new process using execlp
+                if(nig.oprid == 3){
+                    if (execlp("wc","wc", "-w",fileName, NULL) == -1) {
+                        perror("execlp failed");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+
+                if(nig.oprid == 2){
+                    if(execlp("find","find",".","-name",fileName,NULL) == -1){
+                        perror("Find execlp failed");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+            } 
+            
+            else {
+                // This is the server->child process
+                wait(NULL);
+                close(pipe_fd_1[1]); // Close the write end of the pipe (not needed by the parent)
+                char buffer[1024];
+                ssize_t bytesRead;
+                // Read data from the pipe (output of the child process)
+                bytesRead = read(pipe_fd_1[0], buffer, sizeof(buffer));
+                buffer[bytesRead] = '\0';
+                
+                struct message send;
+                if(nig.oprid == 3){
+                    char buffer1[1024];
+                    int count = 0;
+                    int index = 0;
+                    for(int i=0;i<strlen(buffer);++i){
+                        if(buffer[i] >= '0' && buffer[i] <= '9'){
+                            buffer1[index] = buffer[i];
+                            index++;
+                        }
+                        else{
+                            break;
+                        }
+                    }
+                    buffer1[index] = '\0';
+                    send.mtype = nig.mtype;
+                    strcpy(send.mfile,buffer1);
+                    send.oprid = 3;
+                }
+
+                else if(nig.oprid == 2){
+                    send.mtype = nig.mtype;
+                    strcpy(send.mfile,buffer);
+                    printf("\n%s\n",buffer);
+                    send.oprid = 2;
+                }
+
+                msgsnd(msgqid,&send,sizeof(send.mfile),0);
+                //printf("Message sent\n"); 
+
+                close(pipe_fd_1[0]); // Close the read end of the pipe (parent is done reading)
+            }
+        } 
+        else 
+        {
+            // Parent process (server)
+            close(pipe_fd[0]); // Close the read end of the pipe (not needed by the server)
+            write(pipe_fd[1], &nig, sizeof(nig));
+            //printf("Message sent nigg\n");
+            close(pipe_fd[1]); // Close the write end of the pipe (server is done writing)
+            wait(NULL);
+        }
+    }
 
     return 0;
 }
-
